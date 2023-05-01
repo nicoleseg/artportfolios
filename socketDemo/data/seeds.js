6f592dc2e2bd42536c5b2b8ebf3fe56fce0bf9eb
nicoleseg <nicole.segarra23@trinityschoolnyc.org>
2023-05-01 13:37:05 -0400

    yes

--- socketDemo/data/seeds.js
+++ socketDemo/data/seeds.js
@@ -3,7 +3,7 @@ var db = new sqlite3.Database('example.db');
 
 db.run("PRAGMA foreign_keys = ON;"); //enables foreign keys in sqlite3
 
-db.run("INSERT INTO garages (owner) VALUES (?), (?)",
+db.run("INSERT INTO logs (userID,open_at) VALUES (?), (?)",
   'Mrs. Smith',
   'Mr. Wilson',
   function(err) {
--- socketDemo/models/sql_model.js
+++ socketDemo/models/sql_model.js
@@ -1,17 +1,13 @@
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('example.db');
+//change to match derictory
 
 db.run("PRAGMA foreign_keys = ON;"); //enables foreign keys in sqlite3
 //get all garages or any thing that actually is being measured. need to at least be logs.db
-db.all('SELECT * FROM garages', function(err, rows){
-  if(err){
-    console.log(err);
-  } else {
-  	console.log('********GARAGES TABLE**********');
-    console.log(rows);
-  }
-});
-db.all('SELECT * FROM cars', function(err, rows){
+
+//track loggin method that is called backin the OATH controller
+
+db.all('SELECT * FROM logs', function(err, rows){
   if(err){
     console.log(err);
   } else {
@@ -19,25 +15,16 @@ db.all('SELECT * FROM cars', function(err, rows){
     console.log(rows);
   }
 });
-db.run("DELETE FROM garages WHERE id=?", 1, function(err){
+db.run("DELETE FROM logs WHERE id=?", 1, function(err){
   if(err){
     throw err
   }
 });
-db.all('SELECT * FROM garages', function(err, rows){
+db.all('SELECT * FROM logs', function(err, rows){
   if(err){
     console.log(err);
   } else {
   	console.log('********GARAGES TABLE**********');
-    console.log(rows);
-  }
-});
-
-db.all('SELECT * FROM cars', function(err, rows){
-  if(err){
-    console.log(err);
-  } else {
-  	console.log('********CARS TABLE**********');
     console.log(rows);
   }
 });
